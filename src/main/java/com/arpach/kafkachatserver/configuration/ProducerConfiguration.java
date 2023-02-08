1871d1acdc71f1672dcfa8a822f4c564fa20b75e
arun-ekambaram <arunvit1730@gmail.com>
2023-02-08 12:34:51 +0530

    first commit

--- src/main/java/com/arpach/kafkachatserver/configuration/ProducerConfiguration.java
+++ src/main/java/com/arpach/kafkachatserver/configuration/ProducerConfiguration.java
@@ -1,1 +1,47 @@
-package com.arpach.kafkachatserver.ProducerConfiguration;
+package com.arpach.kafkachatserver.configuration;
+
+//import com.arpach.kafkachatserver.constants.KafkaConstants;
+import com.arpach.kafkachatserver.model.Messages;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+@EnableKafka
+@Configuration
+
+public class ProducerConfiguration{
+	@Bean 
+	public  ProducerFactory<String,Messages> producerFactory(){
+		return new DefaultKafkaProducerFactory<>(producerConfigurations());
+	
+	}
+	@Bean
+	public Map<String,Object> producerConfigurations(){
+		Map<String,Object> configurations = new HashMap<>();
+		configurations.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+		configurations.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configurations.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,JsonSerializer.class);
+		return configurations;
+	}
+		
+	@Bean
+	public KafkaTemplate<String,Messages>kafkaTemplate(){
+		return new KafkaTemplate<>(producerFactory());
+	}
+		
+}
+		
+
+
+
